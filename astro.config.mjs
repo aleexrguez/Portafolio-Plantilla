4d1368250aa61796cea7defb38e7c04d48de272c
aleexrguez <alessandro.rodriguezrojas@gmail.com>
2025-05-07 18:03:16 +0200

    fix: cambiar adaptador a vercel/static

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,8 @@ import vercel from "@astrojs/vercel/serverless";
 // If you are not going to use vercel analytics you can delete the analytics
 // If you want to remove vercel from your project you can do npm uninstall vercel
 export default defineConfig({
-  output: "server",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
+  output: 'static',
+  adapter: vercel(),
 });
 
 //If you don't have vercel the configuration will be empty
