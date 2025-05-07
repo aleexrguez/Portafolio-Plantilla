8cb1909bc2c727086d20bb69c680d4f09e5e1cf3
aleexrguez <alessandro.rodriguezrojas@gmail.com>
2025-05-07 17:34:04 +0200

    Primera version del portafolio con plantilla

---
+++ src/const/const.js
@@ -0,0 +1,192 @@
+export const studies = [
+  {
+    title: "Grado medio Técnico en SMR",
+    corporation: "La salle Sagrado Corazón - Madrid ",
+    date: "2020 – 2022",
+  },
+  {
+    title: "Grado Superior Doble Titulación DAM + DAW",
+    corporation: "IES Vírgen de la Paloma - Madrid",
+    date: "2022 – 2025",
+  },
+];
+
+export const IconKeys = {
+  html: "html",
+  css: "css",
+  javaScript: "javaScript",
+  astro: "astro",
+  tailwind: "tailwind",
+  git: "git",
+  github: "github",
+  stripe: "stripe",
+  nodejs: "nodejs",
+  mongodb: "mongodb",
+  json:"json",
+  Bootstrap:"Bootstrap",
+  Xml:"Xml",
+  Symfony:"Symfony",
+  Mysql:"Mysql",
+  Java:"Java",
+  Unity:"Unity",
+  Php:"Php",
+  Csharp:"Csharp",
+  // here add more icon keys
+};
+
+export const skills = [
+  {
+    title: "Html",
+    icon: IconKeys.html,
+  },
+  {
+    title: "Css",
+    icon: IconKeys.css,
+  },
+  {
+    title: "JavaScript",
+    icon: IconKeys.javaScript,
+  },
+  {
+    title: "Astro",
+    icon: IconKeys.astro,
+  },
+  {
+    title: "Tailwindcss",
+    icon: IconKeys.tailwind,
+  },
+
+  {
+    title: "Git",
+    icon: IconKeys.git,
+  },
+
+  {
+    title: "Github",
+    icon: IconKeys.github,
+  },
+  {
+    title: "Node Js",
+    icon: IconKeys.nodejs,
+  },
+
+  {
+    title: "Mongo Db",
+    icon: IconKeys.mongodb,
+  },
+
+  {
+    title: "Xml",
+    icon: IconKeys.Xml,
+  },
+
+  {
+    title: "Json",
+    icon: IconKeys.json,
+  },
+  {
+    title: "Bootstrap",
+    icon: IconKeys.Bootstrap,
+  },
+
+  {
+    title: "Xml",
+    icon: IconKeys.Xml,
+  },
+  {
+    title: "Symfony",
+    icon: IconKeys.Symfony,
+  },
+  {
+    title: "Mysql",
+    icon: IconKeys.Mysql,
+  },
+  {
+    title: "Java",
+    icon: IconKeys.Java,
+  },
+  {
+    title: "Unity",
+    icon: IconKeys.Unity,
+  },
+  {
+    title: "PHP",
+    icon: IconKeys.Php,
+  },
+  {
+    title: "C#",
+    icon: IconKeys.Csharp,
+  },
+
+];
+
+export const projects = [
+  {
+    img: "../../../img/OneLink.png",
+    title: "One Link",
+    link: "https://ecommerce-stripe-763m.vercel.app/",
+    description: `Aplicación tipo Linktree usando Symfony y Tailwind CSS.`,
+    technologies: [
+
+      {
+        icon: IconKeys.html,
+      },
+      {
+        icon: IconKeys.css,
+      },
+      {
+        icon: IconKeys.javaScript,
+      },
+      {
+        icon: IconKeys.tailwind,
+      },
+      {
+        icon: IconKeys.Symfony,
+      },
+      {
+        icon: IconKeys.Mysql,
+      },
+      {
+        icon: IconKeys.stripe,
+      },
+      
+    ],
+  },
+  {
+    img: "../../../img/ArcaneWarrior.png",
+    title: "Arcane Warrior",
+    link: "https://nexanime.vercel.app",
+    description: `Juego 2D creado con Unity y C#.`,
+    technologies: [
+
+      {
+        icon: IconKeys.Unity,
+      },
+      {
+        icon: IconKeys.Csharp,
+      },
+    ],
+  },
+  {
+    img: "../../../img/SRCompany.png",
+    title: "SRCompany",
+    link: "https://preeminent-liger-266356.netlify.app/",
+    description: `App web para pedidos y pagos digitales en
+                  restaurantes, desarrollada como proyecto de TFG.`,
+    technologies: [
+      {
+        icon: IconKeys.html,
+      },
+      {
+        icon: IconKeys.css,
+      },
+      {
+        icon: IconKeys.javaScript,
+      },
+      {
+        icon: IconKeys.Csharp,
+      },
+    ],
+  },
+
+];
